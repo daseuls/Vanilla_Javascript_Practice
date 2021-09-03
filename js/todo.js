e3a55fe4a06c841d3097e74447d9b76653801cb9
daseuls <dls2tmfs2@naver.com>
2021-09-03 14:30:17 +0900

    Feat: delete Todo List

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,39 @@
+// const toDoForm = document.getElementById("todo-form")
+// const toDoList = document.getElementById("todo-list")
+// const toDoInput = document.querySelector("#todo-form input")
+
+// function paintTodo(toDoInputValue) {
+//   const li = document.createElement("li")
+//   const span = document.createElement("span")
+//   toDoList.appendChild(li)
+//   li.appendChild(span)
+//   span.innerText = toDoInputValue
+// }
+
+// function handleToDoSumbit(e) {
+//   e.preventDefault()
+//   const toDoInputValue = toDoInput.value
+//   toDoInput.value = ""
+//   paintTodo(toDoInputValue)
+// }
+
+// toDoForm.addEventListener("submit", handleToDoSumbit)
+
 const toDoForm = document.getElementById("todo-form")
 const toDoList = document.getElementById("todo-list")
 const toDoInput = document.querySelector("#todo-form input")
+const deleteButton = document.querySelector("#todo-list button")
 
 function paintTodo(toDoInputValue) {
-  const toDoListEach = document.createElement("li")
-  toDoList.appendChild(toDoListEach)
-  toDoListEach.innerText = toDoInputValue
+  const li = document.createElement("li")
+  const span = document.createElement("span")
+  span.innerText = toDoInputValue
+  const button = document.createElement("button")
+  button.innerText = "❌"
+  button.addEventListener("click", handleDeleteToDo)
+  toDoList.appendChild(li)
+  li.appendChild(span)
+  li.appendChild(button)
 }
 
 function handleToDoSumbit(e) {
@@ -13,6 +41,12 @@ function handleToDoSumbit(e) {
   const toDoInputValue = toDoInput.value
   toDoInput.value = ""
   paintTodo(toDoInputValue)
+}
+
+function handleDeleteToDo(e) {
+  console.log(e.target.parentNode)
+  console.log(e.target.parentElement)
+  e.target.parentElement.remove()
 }
 
 toDoForm.addEventListener("submit", handleToDoSumbit)
