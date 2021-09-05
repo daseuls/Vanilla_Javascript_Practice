00d32949b7e43267dd20d2f4808958076c1a9100
daseuls <dls2tmfs2@naver.com>
2021-09-05 17:10:48 +0900

    Feat: delete localStorage toDoList

--- js/todo.js
+++ js/todo.js
@@ -13,10 +13,11 @@ function saveToDos() {
 
 // input의 값을 담은 toDoInputValue를 인자로 받아서 li와 span태그를 만들고 그 안에 그 값을 넣기
 // button 태그를 만들어서 button 태그를 클릭하면 handleDeleteToDo 함수 실행하기
-function paintTodo(toDoInputValue) {
+function paintTodo(newToDoObj) {
   const li = document.createElement("li")
+  li.id = newToDoObj.id
   const span = document.createElement("span")
-  span.innerText = toDoInputValue
+  span.innerText = newToDoObj.text
   const button = document.createElement("button")
   button.innerText = "❌"
   button.addEventListener("click", handleDeleteToDo)
@@ -32,14 +33,20 @@ function handleToDoSubmit(e) {
   e.preventDefault()
   const toDoInputValue = toDoInput.value
   toDoInput.value = ""
-  toDos.push(toDoInputValue) // 빈 배열에 input value 값 넣기
-  paintTodo(toDoInputValue) // input value값을 그려내는 함수 실행
+  const newToDoObj = {
+    text: toDoInputValue,
+    id: Date.now(),
+  }
+  toDos.push(newToDoObj) // 빈 배열에 input value 값 넣기
+  paintTodo(newToDoObj) // input value값을 그려내는 함수 실행
   saveToDos() // 로컬스토리지에 toDos에 있는 값 json화 해서 저장
 }
 
 // 버튼을 클릭했을때 실행되는 함수, 인자의 target을 이용해 그 이벤트가 발생한 부모의 값을 지우는 역할
 function handleDeleteToDo(e) {
   e.target.parentElement.remove()
+  toDos = toDos.filter((el) => el.id !== parseInt(e.target.parentElement.id))
+  saveToDos()
 }
 
 // form에 submit 이벤트가 발생했을때 handleToDoSubmit을 발생시킨다.
